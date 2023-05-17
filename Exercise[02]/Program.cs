1f99595b1c970ab7dfaff4adcb2c042d382f2a49
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-05-17 22:31:28 +0300

    ApplayChanges

---
+++ Exercise[02]/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+namespace Exercise_02_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultPairs = Calculate.Sum(
+                new()
+                {
+                    Term.Create(2 ,2),
+                    Term.Create(1 ,3),
+                    Term.Create(0 ,1),
+                },
+            new()
+                {
+                    Term.Create(2 ,3),
+                    Term.Create(1 ,3),
+                    Term.Create(0 ,0),
+                }
+            ).FirstOrDefault();
+
+            var result = resultPairs.Value.ToString().Trim().Replace(" ", " + ");
+            Console.WriteLine(result);
+            Calculate.GetRoot(resultPairs.Key);
+        }
+    }
+}
+public class Calculate
+{
+    public static Dictionary<List<Term>, StringBuilder> Sum(List<Term> firstEquation, List<Term> secondEquation)
+    {
+        StringBuilder resultString = new();
+        List<Term> resultEquation = new();
+        if (!firstEquation.Count.Equals(secondEquation.Count))
+            return new() { { null!, resultString.Append("two equations not degree-two equations !") } };
+
+        for (int i = 0 ; i < firstEquation.Count ; i++)
+            for (int j = 0 ; j < secondEquation.Count ; j++)
+            {
+                if (firstEquation[i].Equals(secondEquation[j]))
+                {
+                    Term newTerm = Term.Create(
+                        firstEquation[i].Exponent,
+                        firstEquation[i].Coefficient + secondEquation[j].Coefficient);
+                    resultEquation.Add(newTerm);
+
+                    resultString.Append($"{newTerm.Coefficient}X^{newTerm.Exponent} ");
+                }
+            }
+        return new() { { resultEquation, resultString } };
+    }
+
+
+    public static void GetRoot(List<Term> equation)
+    {
+        double a = 0, b = 0, c = 0;
+        equation.ForEach(t =>
+        {
+            if (t.Exponent.Equals(2))
+                a = t.Coefficient;
+            if (t.Exponent.Equals(1))
+                b = t.Coefficient;
+            if (t.Exponent.Equals(0))
+                c = t.Coefficient;
+        });
+        double discriminant = Math.Pow(b, 2) - (4 * (a * c));
+
+        switch (discriminant)
+        {
+            case 0:
+                Console.WriteLine($"one solution {-1 * b / (2 * a)}");
+                break;
+            case > 0:
+                Console.WriteLine($"first solution: " +
+                    $"{(-1 * b) + (Math.Sqrt(discriminant) / (2 * a))}");
+                Console.WriteLine($"second solution: " +
+                    $"{(-1 * b) - (Math.Sqrt(discriminant) / (2 * a))}");
+                break;
+            case < 0:
+                Console.WriteLine("no solution !");
+                break;
+        }
+    }
+}
---
+++ Exercise[02]/Term.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+public struct Term
+{
+    private Term(double exponent = 0, double coefficient = 0)
+    {
+        Exponent = exponent;
+        Coefficient = coefficient;
+    }
+    public double Exponent { get; private set; }
+    public double Coefficient { get; private set; }
+    public static Term Create(double exponent = 0, double coefficient = 0) =>
+        new(exponent, coefficient);
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+            return false;
+
+        return this.Exponent.Equals(((Term)obj).Exponent);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string? ToString()
+    {
+        return base.ToString();
+    }
+}

30ba2cda5ac1ae860304c00a9695e8686190ab57
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-05-22 22:12:09 +0300

    ApplayChanges

---
+++ Exercise[03]/Balance.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise_03_;
+
+public static class Balance
+{
+    static Dictionary<char, char> pairs = new()
+    {
+        { '<', '>' },
+        { '{', '}' },
+        { '(', ')' },
+        { '[', ']' },
+    };
+    public static bool IsBalanced(this string text)
+    {
+        Stack<char> brackets = new();
+        try
+        {
+            foreach (char t in text)
+            {
+                if (pairs.Keys.Contains(t))
+                    brackets.Push(t);
+                else
+                    if (pairs.Values.Contains(t))
+                {
+                    if (t == pairs[brackets.Peek()])
+                        brackets.Pop();
+                    else
+                        return false;
+                }
+                else
+                    continue;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+        return brackets.Count() == 0 ? true : false;
+    }
+}
---
+++ Exercise[03]/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise_03_;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
